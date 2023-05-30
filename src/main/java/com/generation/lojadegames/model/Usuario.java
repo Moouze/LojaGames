2b72f4eea8a664ee3528ac3fcdb18f91e7d70b6f
Lucas <lucas_dev@outlook.com.br>
2023-05-30 10:55:39 -0300

    Add Usuario

---
+++ src/main/java/com/generation/lojadegames/model/Usuario.java
@@ -0,0 +1,63 @@
+package com.generation.lojadegames.model;
+
+public class Usuario {
+    private Long id;
+
+    private String nome;
+
+    private String usuario;
+
+    private String senha;
+
+    private String foto;
+
+    private String tipoUsuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
